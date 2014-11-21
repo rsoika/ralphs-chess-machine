d4d097edcdb92e0ce324ae9d601aaf6f68634a0b
Ralph Soika <ralph.soika@imixs.com>
2014-11-21 18:04:18 +0100

    initalsetup

---
+++ src/main/java/com/soika/chess/rcm.java
@@ -0,0 +1,11 @@
+package com.soika.chess;
+
+public class rcm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Chess Maschine!");
+	}
+
+}
